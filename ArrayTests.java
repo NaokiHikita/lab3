3c734772f35732dda00c81867f6f3fc6fea3f8cc
NaokiHikita <nhikita@ucsd.edu>
2023-10-23 13:18:57 -0700

    Update ArrayExamples.java and ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlaceFail() {
+    int[] input1 = {1, 2};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{2,1}, input1);
+  }
+
+  @Test
+  public void testReversedFail() {
+    int[] input1 = {1};
+    assertArrayEquals(new int[]{1}, ArrayExamples.reversed(input1));
+  }
 }

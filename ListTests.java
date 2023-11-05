d82d86dc8eb308f6970a7cd58f38c6acbaf00043
NaokiHikita <nhikita@ucsd.edu>
2023-11-05 15:04:55 -0800

    Change

---
+++ ListTests.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ListTests {
+	@Test 
+	public void testFilter() {
+    List<String> input1 = new ArrayList<>();
+    input1.add("Hello");
+    input1.add("Hello");
+    assertArrayEquals(input1, ListExamples.filter(input1, StringChecker sc);
+	}
+}
